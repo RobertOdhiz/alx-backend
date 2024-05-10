f044b70ad47bf6e559974f2d4e77f34032709915
Robert Odhiambo <odhizrober254@gmail.com>
2024-05-10 21:44:41 +0300

    Basic Cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Basic Cache Module
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache class inherits from BaseCaching """
+    def put(self, key, item):
+        """ Assigns self.cache_data the item value for the key """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Returns the value in self.cache_data dictionary linked to key """
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
